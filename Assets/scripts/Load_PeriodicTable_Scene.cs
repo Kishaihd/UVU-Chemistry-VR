6a51973afc60a48ca35e25b488d6957592bd0098
kaden <mr.pinstripe_Suit@hotmail.com>
2018-07-06 17:19:26 -0600

    Loading Scene
    Click on desktop screen to load large periodic table scene.

---
+++ Assets/scripts/Load_PeriodicTable_Scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNewScene : MonoBehaviour {
+    string sceneToLoad;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("LoadNewScene Script started.");
+       
+        Application.LoadLevel(name: "PeriodicTable_Scene");
+    }
+	
+
+}
