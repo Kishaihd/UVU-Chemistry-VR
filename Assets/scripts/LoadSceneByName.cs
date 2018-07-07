e171898dcdec1b8f3961fe14d8adaefdff33acc8
kaden <mr.pinstripe_Suit@hotmail.com>
2018-07-06 18:51:32 -0600

    Multi-stage Periodic Table
    Loading new scenes from interactions with current scene.

---
+++ Assets/scripts/LoadSceneByName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_BigPencil_Scene : MonoBehaviour {
+
+	// Use this for initialization
+	//void Start () {
+
+	//}
+
+    void LoadLevel(string levelName)
+    {
+        Application.LoadLevel(name: levelName);
+    }
+	
+}
