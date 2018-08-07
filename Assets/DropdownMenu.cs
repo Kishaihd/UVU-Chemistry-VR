9ae050b27484539a1b8cd794f7ba159e6d77cbcc
Unknown <mr.pinstripe_Suit@hotmail.com>
2018-08-07 03:50:53 -0600

    Glo tasks completed.
    Carbon lattice scene - scene change, vibration.
    Big pencil scene - scene change, objective marker.
    Atomic scene - fixed camera.

---
+++ Assets/DropdownMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropdownMenu : MonoBehaviour {
+
+    public List<GameObject> lessonObjectives;
+
+	// Use this for initialization
+	void Start () {
+        lessonObjectives.AddRange(GameObject.FindGameObjectsWithTag("lessonObjectives"));
+	}
+	
+    public void Dropdown_IndexChanged(int index)
+    {
+        foreach (GameObject item in lessonObjectives)
+        {
+            item.SetActive(false);
+        }
+        lessonObjectives[index].SetActive(true);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
