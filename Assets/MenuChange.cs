9c54f048fe5db7683fd2e2dff77d3c8424dacf43
Unknown <danielthegray11@gmail.com>
2018-08-07 22:09:44 -0600

    Menu Script
    menu script misbehaving

---
+++ Assets/MenuChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuChange : MonoBehaviour {
+    public List<GameObject> sceneObjectives;
+    public Dropdown dropdown;
+    public GameObject lessonObjectives;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SelectLesson();
+	}
+
+    void SelectLesson()
+    {
+        sceneObjectives.AddRange(GameObject.FindGameObjectsWithTag("lessonObjective"));
+    }
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        foreach (GameObject item in sceneObjectives)
+        {
+            item.SetActive(false);
+        }
+        sceneObjectives[index].SetActive(true);
+    }
+
+	
+}
