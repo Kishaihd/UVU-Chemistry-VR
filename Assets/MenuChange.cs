c84d8c6f394968271c8cd2998ff2c29be390f49e
Unknown <danielthegray11@gmail.com>
2018-08-14 22:03:46 -0600

    Got menu drop down to work!
    Yay

--- Assets/MenuChange.cs
+++ Assets/MenuChange.cs
@@ -6,27 +6,43 @@ using UnityEngine.UI;
 public class MenuChange : MonoBehaviour {
     public List<GameObject> sceneObjectives;
     public Dropdown dropdown;
-    public GameObject lessonObjectives;
+    //public GameObject lessonObjectives;
 
-	// Use this for initialization
-	void Start ()
+    //public void SelectLesson(Dropdown myList, List<GameObject> optionsArray)
+    //{
+       
+    //    //Debug.Log(options[0]);
+    //}
+    // Use this for initialization
+    void Start ()
     {
-        SelectLesson();
-	}
-
-    void SelectLesson()
-    {
+        List<string> options = new List<string>();
         sceneObjectives.AddRange(GameObject.FindGameObjectsWithTag("lessonObjective"));
+        //SelectLesson(dropdown, sceneObjectives); 
+        foreach (var option in sceneObjectives)
+        {
+            options.Add(option.name);
+        }
+        //options.Add("Select Lesson");
+        options.Reverse();
+        sceneObjectives.Reverse();
+        dropdown.ClearOptions();
+        dropdown.AddOptions(options);
+        dropdown.value = sceneObjectives.IndexOf(GameObject.Find("Select Lesson"));
+        Dropdown_IndexChanged(sceneObjectives.IndexOf(GameObject.Find("Select Lesson")));
+        //Debug.Log(sceneObjectives[0]);
     }
+
 
     public void Dropdown_IndexChanged(int index)
     {
         foreach (GameObject item in sceneObjectives)
         {
             item.SetActive(false);
+            Debug.Log(item.gameObject.name);
         }
         sceneObjectives[index].SetActive(true);
     }
 
-	
+
 }
