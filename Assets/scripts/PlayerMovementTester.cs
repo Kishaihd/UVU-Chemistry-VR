426693d8908c2d9844619ac4b6f44244e38d1c02
Unknown <stormymtn@gmail.com>
2018-09-06 14:19:32 -0600

    scene instantiation working!

---
+++ Assets/scripts/PlayerMovementTester.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTester : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 newPosition;
+    public float Speed = 10f;
+    public float Gravity = 9.81f;
+    public float JumpSpeed = 10f;
+    public bool CanRun = true;
+
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+        {
+            newPosition.y = JumpSpeed;
+        }
+
+        newPosition.y -= Gravity;
+
+        if (CanRun)
+        {
+            newPosition.x = Speed * Input.GetAxis("Horizontal");
+            newPosition.z = Speed * Input.GetAxis("Vertical");
+        }
+        controller.Move(newPosition * Time.deltaTime);
+	}
+}
--- Assets/scripts/SceneSetUp.cs
+++ Assets/scripts/SceneSetUp.cs
@@ -7,97 +7,92 @@ using UnityEngine.UI;
 
 public class SceneSetUp : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject centerLightPrefab;
-    [SerializeField]
-    private GameObject playerPrefab;
-    [SerializeField]
-    private GameObject[] perimeterLightPrefab;
-    [SerializeField]
-    private GameObject[] sceneryPrefab;
-    [SerializeField]
-    private GameObject[] labEquipmentPrefab;
-    [SerializeField]
-    private GameObject[] screenPrefab;
+    public GameObject centerLightPrefab;
 
-    private GameObject centerLight;
-    private GameObject player;
-    private GameObject[] perimeterLight;
-    private GameObject[] scenery;
-    private GameObject[] labEquipment;
-    private GameObject[] screen;
+    public GameObject playerPrefab;
+
+    public GameObject[] perimeterLightPrefabs;
+    public GameObject[] perimeterLights;
+
+    public GameObject[] sceneryPrefabs;
+    public GameObject[] scenerys;
+
+    public GameObject[] labEquipmentPrefabs;
+    public GameObject[] labEquipments;
+
+    public GameObject[] screenPrefabs;
+    public GameObject[] screens;
 
     public int buildindex { get; private set; }
 
     // Use this for initialization
     void Start ()
     {
-        if (centerLight == null)
-        {
-            CenterLight();
-        }
+        CenterLight();
+        
+        LabEquipment();
 
-        if (labEquipment[0] == null)
-        {
-            LabEquipment();
-        }
+        Player();
 
-        if (player == null)
-        {
-            Player();
-        }
+        Scenery();
+
+        Screen();
 
         if (buildindex == 0)
         {
             PerimeterLight();
         }
-        else
-        {
-            Screen();
-        }
-
 	}
 
     private void CenterLight()
     {
-        centerLight = GameObject.FindWithTag(tag: "Center_Light");
-        Instantiate(centerLightPrefab, centerLight.transform.position, centerLight.transform.rotation);
+        Instantiate(centerLightPrefab, centerLightPrefab.transform.position, centerLightPrefab.transform.rotation);
+        //Debug.Log("CenterLight on!");
     }
         
     private void PerimeterLight()
     {
-        int x = 0;
-        if (x < 8)
-        perimeterLight[x] = GameObject.FindWithTag(tag: "Perimeter_Light");
-        Instantiate(perimeterLightPrefab[x], perimeterLight[x].transform.position, perimeterLight[x].transform.rotation);
-        x++;
+        perimeterLights = new GameObject[perimeterLightPrefabs.Length];
+        for (int i = 0; i < perimeterLightPrefabs.Length; i++)
+        {
+            perimeterLights[i] = Instantiate(perimeterLightPrefabs[i]) as GameObject;
+            //Debug.Log("perimeter light:" + i);
+        }
     }
 
     private void LabEquipment()
     {
-        int y = 0;
-        if (y < 40)
+        labEquipments = new GameObject[labEquipmentPrefabs.Length];
+        for (int i = 0; i < labEquipmentPrefabs.Length; i++)
         {
-            labEquipment[y] = GameObject.FindWithTag(tag: "Lab_Equipment");
-            Instantiate(labEquipmentPrefab[y], labEquipment[y].transform.position, labEquipment[y].transform.rotation);
-            y++;
+            labEquipments[i] = Instantiate(labEquipmentPrefabs[i]) as GameObject;
+            //Debug.Log("lab equipment #:" + i);
         }
-    }
+    } 
 
     private void Player()
     {
-        player = GameObject.FindWithTag(tag: "Player");
-        Instantiate(playerPrefab, player.transform.position, player.transform.rotation);
+        Instantiate(playerPrefab, playerPrefab.transform.position, playerPrefab.transform.rotation);
+        //Debug.Log("player");
     }
 
     private void Screen()
     {
-        int z = 0;
-        if (z < 4)
+        screens = new GameObject[screenPrefabs.Length];
+        for (int i = 0; i < screenPrefabs.Length; i++)
         {
-            screen[z] = GameObject.FindWithTag(tag: "Interior_Screen");
-            Instantiate(screenPrefab[z], labEquipment[z].transform.position, labEquipment[z].transform.rotation);
-            z++;
+            screens[i] = Instantiate(screenPrefabs[i]) as GameObject;
+            //Debug.Log("screen:" + i);
+        }
+    }
+
+    private void Scenery()
+    {
+        scenerys = new GameObject[sceneryPrefabs.Length];
+        for (int i = 0; i < sceneryPrefabs.Length; i++)
+        {
+            scenerys[i] = Instantiate(sceneryPrefabs[i]) as GameObject;
+            //Debug.Log("scenery:" + i);
         }
     }
 }
