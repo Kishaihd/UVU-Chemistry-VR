626ed7adb9b614bbddaad67a7286a97afb2805b2
Unknown <stormymtn@gmail.com>
2018-09-21 01:45:08 -0600

    updated prefabs for scenes 1-4 and scenescript

--- Assets/scripts/SceneSetUp.cs
+++ Assets/scripts/SceneSetUp.cs
@@ -37,6 +37,22 @@ public class SceneSetUp : MonoBehaviour
     public GameObject[] carbonLatticePrefabs;
     public GameObject[] carbons;
 
+    public GameObject[] carbon12AtomGroupPrefabs;
+    public GameObject[] carbon12s;
+
+    public GameObject[] electronGroupPrefabs;
+    public GameObject[] electrons;
+
+    public GameObject[] protonGroupPrefabs;
+    public GameObject[] protons;
+
+    public GameObject[] neutronGroupPrefabs;
+    public GameObject[] neutrons;
+
+    public GameObject[] specialLightPrefabs;
+    public GameObject[] specialLights;
+      
+
     private int sceneID;
     //private AssetBundle scenes;
     //public int buildindex { get; private set; }
@@ -62,6 +78,7 @@ public class SceneSetUp : MonoBehaviour
         Scenery();
 
         Screen();
+
         Debug.Log("Scene ID: ");
         Debug.Log(sceneID);
         
@@ -82,7 +99,20 @@ public class SceneSetUp : MonoBehaviour
             CarbonLattice();
         }
 
+        if (sceneID == 3)
+        {
+            Carbon12AtomGroup();
+            ElectronGroup();
+            ProtonGroup();
+            NeutronGroup();
+            SpecialLights();
+        }
 
+        if (sceneID == 4)
+        {
+            
+        //carbon isotopes models - this section needs to be setup
+        }
 
     }
 
@@ -98,8 +128,11 @@ public class SceneSetUp : MonoBehaviour
 
     private void CenterLight()
     {
-        Instantiate(centerLightPrefab, centerLightPrefab.transform.position, centerLightPrefab.transform.rotation);
+        if (sceneID != 3)
+        { 
+            Instantiate(centerLightPrefab, centerLightPrefab.transform.position, centerLightPrefab.transform.rotation);
         //Debug.Log("CenterLight on!");
+        }
     }
 
     private void PerimeterLight()
@@ -173,5 +206,55 @@ public class SceneSetUp : MonoBehaviour
             carbons[i] = Instantiate(carbonLatticePrefabs[i]) as GameObject;
             //Debug.Log("carbon lattice object:" + i);
         }
+    }
+
+    private void Carbon12AtomGroup()
+    {
+        carbon12s = new GameObject[carbon12AtomGroupPrefabs.Length];
+        for (int i = 0; i < carbon12AtomGroupPrefabs.Length; i++)
+        {
+            carbon12s[i] = Instantiate(carbon12AtomGroupPrefabs[i]) as GameObject;
+            //Debug.Log("carbon 12 atom object:" + i);
+        }
+    }
+
+    private void ElectronGroup()
+    {
+        electrons = new GameObject[electronGroupPrefabs.Length];
+        for (int i = 0; i < electronGroupPrefabs.Length; i++)
+        {
+            electrons[i] = Instantiate(electronGroupPrefabs[i]) as GameObject;
+            //Debug.Log("electron object:" + i);
+        }
+    }
+
+    private void ProtonGroup()
+    {
+            protons = new GameObject[protonGroupPrefabs.Length];
+            for (int i = 0; i < protonGroupPrefabs.Length; i++)
+            {
+                protons[i] = Instantiate(protonGroupPrefabs[i]) as GameObject;
+                //Debug.Log("carbon 12 atom object:" + i);
+            }
+    }
+
+    private void NeutronGroup()
+    {
+                neutrons = new GameObject[neutronGroupPrefabs.Length];
+                for (int i = 0; i < neutronGroupPrefabs.Length; i++)
+                {
+                    neutrons[i] = Instantiate(neutronGroupPrefabs[i]) as GameObject;
+                    //Debug.Log("carbon 12 atom object:" + i);
+                }
+    }
+
+    private void SpecialLights()
+    {
+                specialLights = new GameObject[specialLightPrefabs.Length];
+                for (int i = 0; i < specialLightPrefabs.Length; i++)
+                {
+                    specialLights[i] = Instantiate(specialLightPrefabs[i]) as GameObject;
+                    //Debug.Log("carbon 12 atom object:" + i);
+                }
     }
 }
