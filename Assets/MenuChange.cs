1a7c3a5e6140bd429987da68d810bcf6e571f9be
kaden <mr.pinstripe_Suit@hotmail.com>
2018-10-29 15:42:31 -0600

    Objectives Menu
    Updated the script for the objectives menu. The drop down should change the objectives again.

---
+++ Assets/MenuChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuChange : MonoBehaviour {
+    public List<GameObject> sceneObjectives;
+    public Dropdown dropdown;
+
+	// Use this for initialization
+	void Start () {
+        List<string> options = new List<string>();
+        sceneObjectives.AddRange(GameObject.FindGameObjectsWithTag("lessonObjective"));
+
+        foreach (var option in sceneObjectives)
+        {
+            options.Add(option.name);
+        }
+        options.Reverse();
+        sceneObjectives.Reverse();
+        dropdown.ClearOptions();
+        dropdown.AddOptions(options);
+        dropdown.value = sceneObjectives.IndexOf(GameObject.Find("Select Lesson"));
+        Dropdown_IndexChanged(sceneObjectives.IndexOf(GameObject.Find("Select Lesson")));
+	}
+	
+	public void Dropdown_IndexChanged(int index)
+    {
+        foreach (GameObject item in sceneObjectives)
+        {
+            item.SetActive(false);
+            Debug.Log(item.gameObject.name);
+        }
+        sceneObjectives[index].SetActive(true);
+    }
+}
